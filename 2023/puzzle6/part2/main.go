a067857a060845682030ba4735fec592f8ca3a6a
Tobias Theel <theel@denic.de>
2023-12-07 16:00:50 +0100

    cleanup

--- 2023/puzzle6/part2/main.go
+++ 2023/puzzle6/part2/main.go
@@ -1,30 +1,21 @@
 package main
 
 func main() {
-	times := []int{56977793}
-	distances := []int{499221010971440}
+	time := 56977793
+	distances := 499221010971440
 
-	result := 0
-	for j, time := range times {
-		waysToWin := 0
-		for i := 0; i < time; i++ {
-			if i == 0 {
-				continue
-			}
-
-			distance := (time - i) * i
-
-			if distance > distances[j] {
-				waysToWin++
-			}
+	waysToWin := 0
+	for i := 0; i < time; i++ {
+		if i == 0 {
+			continue
 		}
 
-		if result == 0 {
-			result = waysToWin
-		} else {
-			result *= waysToWin
+		distance := (time - i) * i
+
+		if distance > distances {
+			waysToWin++
 		}
 	}
 
-	println(result)
+	println(waysToWin)
 }
